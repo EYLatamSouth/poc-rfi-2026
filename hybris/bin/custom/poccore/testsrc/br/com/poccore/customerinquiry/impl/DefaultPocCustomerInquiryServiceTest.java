2363fb9faf5e7d82e2c5e91e37cec275ec0dd1f9
Matheus Lemes <Matheus.Lemes@br.ey.com>
2026-02-25 17:36:59 -0300

    [US-0] fix: locale question

--- hybris/bin/custom/poccore/testsrc/br/com/poccore/customerinquiry/impl/DefaultPocCustomerInquiryServiceTest.java
+++ hybris/bin/custom/poccore/testsrc/br/com/poccore/customerinquiry/impl/DefaultPocCustomerInquiryServiceTest.java
@@ -4,6 +4,7 @@ import br.com.poc.occ.dto.user.product.PocProductQuestionWsDTO;
 import br.com.poccore.enums.CustomerInquiryApprovalStatus;
 import br.com.poccore.model.CustomerProductInquiryModel;
 import de.hybris.bootstrap.annotations.UnitTest;
+import de.hybris.platform.commerceservices.i18n.CommerceCommonI18NService;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.core.model.user.CustomerModel;
 import de.hybris.platform.servicelayer.model.ModelService;
@@ -14,9 +15,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Locale;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @UnitTest
 @RunWith(MockitoJUnitRunner.class)
@@ -28,9 +31,13 @@ public class DefaultPocCustomerInquiryServiceTest {
     @Mock
     private ModelService modelService;
 
+    @Mock
+    private CommerceCommonI18NService commerceCommonI18NService;
+
     @Before
     public void setUp() {
         pocCustomerInquiryService.setModelService(modelService);
+        pocCustomerInquiryService.setCommerceCommonI18NService(commerceCommonI18NService);
     }
 
     @Test
@@ -42,12 +49,15 @@ public class DefaultPocCustomerInquiryServiceTest {
         CustomerModel customerModel = new CustomerModel();
         PocProductQuestionWsDTO questionWsDTO = new PocProductQuestionWsDTO();
         questionWsDTO.setQuestion("Dummy question?");
+        Locale loc = Locale.of("BR");
+        when(commerceCommonI18NService.getCurrentLocale()).thenReturn(loc);
 
         CustomerProductInquiryModel response = pocCustomerInquiryService.createCustomerInquiry(productModel, questionWsDTO, customerModel);
+        verify(commerceCommonI18NService, times(1)).getCurrentLocale();
         assertNotNull(response);
         assertEquals(customerModel, response.getCustomer());
         assertEquals(productModel, response.getProduct());
-        assertEquals(questionWsDTO.getQuestion(), response.getQuestion());
+        assertEquals(questionWsDTO.getQuestion(), response.getQuestion(loc));
         assertEquals(CustomerInquiryApprovalStatus.PENDING, response.getApprovalStatus());
     }
 }
