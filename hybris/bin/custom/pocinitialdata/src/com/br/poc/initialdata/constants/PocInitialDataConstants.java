3186a820ad66fea4e231cd9a6b9341c4d97cd086
Alexandre Feher <alexandre.feher@br.ey.com>
2026-02-26 08:47:22 -0300

    [US-00] feat: criação da extension de initialdata

---
+++ hybris/bin/custom/pocinitialdata/src/com/br/poc/initialdata/constants/PocInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.br.poc.initialdata.constants;
+
+/**
+ * Global class for all PocInitialData constants.
+ */
+public final class PocInitialDataConstants extends GeneratedPocInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "pocinitialdata";
+
+	private PocInitialDataConstants()
+	{
+		//empty
+	}
+}
