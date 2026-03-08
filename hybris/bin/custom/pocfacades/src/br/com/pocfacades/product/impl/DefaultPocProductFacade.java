2901827536b70edb23c160b40b1a584195d3d803
Rodolfo Miao <rodolfo.miao@br.ey.com>
2026-03-08 18:05:30 -0300

    [US-05] feat: Remover restricao de busca

--- hybris/bin/custom/pocfacades/src/br/com/pocfacades/product/impl/DefaultPocProductFacade.java
+++ hybris/bin/custom/pocfacades/src/br/com/pocfacades/product/impl/DefaultPocProductFacade.java
@@ -3,13 +3,21 @@ package br.com.pocfacades.product.impl;
 import br.com.poccore.service.PocProductService;
 import br.com.pocfacades.product.PocProductFacade;
 import br.com.vivo.facades.product.data.PocProductEngagementSummaryInfoData;
+import de.hybris.platform.search.restriction.SearchRestrictionService;
 
 public class DefaultPocProductFacade implements PocProductFacade {
 
+    private SearchRestrictionService searchRestrictionService;
     private PocProductService pocProductService;
 
     public PocProductEngagementSummaryInfoData getEngagementSummary(String productCode){
-       return getPocProductService().getEngagementSummary(productCode);
+        searchRestrictionService.disableSearchRestrictions();
+        PocProductEngagementSummaryInfoData pocProductEngagementSummaryInfoData;
+        pocProductEngagementSummaryInfoData = getPocProductService().getEngagementSummary(productCode);
+        searchRestrictionService.enableSearchRestrictions();
+
+        return pocProductEngagementSummaryInfoData;
+
     }
 
     public PocProductService getPocProductService() {
@@ -19,5 +27,14 @@ public class DefaultPocProductFacade implements PocProductFacade {
     public void setPocProductService(PocProductService pocProductService) {
         this.pocProductService = pocProductService;
     }
+
+    public SearchRestrictionService getSearchRestrictionService() {
+        return searchRestrictionService;
+    }
+
+    public void setSearchRestrictionService(SearchRestrictionService searchRestrictionService) {
+        this.searchRestrictionService = searchRestrictionService;
+    }
+
 
 }
