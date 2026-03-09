c3a6ff3e8aa69f43f00ab9089850cc7332d0fcbe
joao-caillot-ey <joao.caillot@br.ey.com>
2026-03-09 10:05:29 -0300

    [US-01] feat: Teste unitário

--- hybris/bin/custom/pococc/testsrc/br/com/pococc/occ/controllers/PocProductsControllerTest.java
+++ hybris/bin/custom/pococc/testsrc/br/com/pococc/occ/controllers/PocProductsControllerTest.java
@@ -4,12 +4,16 @@ import br.com.poc.occ.dto.product.PocProductEngagementSummaryWsDTO;
 import br.com.pocfacades.product.PocProductFacade;
 import br.com.vivo.facades.product.data.PocProductEngagementSummaryInfoData;
 import de.hybris.bootstrap.annotations.UnitTest;
+import de.hybris.platform.commercefacades.product.ProductFacade;
+import de.hybris.platform.commercefacades.product.data.ReviewData;
+import de.hybris.platform.commercewebservicescommons.dto.product.ReviewWsDTO;
 import de.hybris.platform.webservicescommons.mapping.DataMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.validation.Validator;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -26,6 +30,12 @@ public class PocProductsControllerTest {
 	private PocProductFacade pocProductFacade;
 
 	@Mock
+	private ProductFacade productFacade;
+
+	@Mock
+	private Validator reviewDTOValidator;
+
+	@Mock
 	private DataMapper dataMapper;
 
 	@Test
@@ -38,5 +48,21 @@ public class PocProductsControllerTest {
 		verify(pocProductFacade, times(1)).getEngagementSummary(productCode);
 		verify(dataMapper, times(1))
 			.map(any(PocProductEngagementSummaryInfoData.class), eq(PocProductEngagementSummaryWsDTO.class));
+	}
+
+	@Test
+	public void testCreateProductReview() {
+		String productCode = "productCode";
+		ReviewWsDTO reviewWsDTO = new ReviewWsDTO();
+		reviewWsDTO.setHeadline("headline");
+		reviewWsDTO.setComment("comment");
+		reviewWsDTO.setRating(4.0);
+		doNothing().when(reviewDTOValidator).validate(any(), any());
+		when(dataMapper.map(any(ReviewWsDTO.class), eq(ReviewData.class), any(String.class)))
+			.thenReturn(new ReviewData());
+
+		pocProductsController.createProductReview(productCode, reviewWsDTO, null);
+
+		verify(productFacade, times(1)).postReview(eq(productCode), any(ReviewData.class));
 	}
 }
