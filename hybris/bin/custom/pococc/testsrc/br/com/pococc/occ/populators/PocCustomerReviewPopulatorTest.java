b4e548b48dd3c14f9e03f189e4ab10caebcc1ec7
tiago.gueto <tiago.gueto@studio.ey.com>
2026-03-10 10:42:35 -0300

    [US-02] feat: Simplificando teste.

--- hybris/bin/custom/pococc/testsrc/br/com/pococc/occ/populators/PocCustomerReviewPopulatorTest.java
+++ hybris/bin/custom/pococc/testsrc/br/com/pococc/occ/populators/PocCustomerReviewPopulatorTest.java
@@ -31,52 +31,16 @@ public class PocCustomerReviewPopulatorTest {
     private CustomerReviewModel source;
 
     private ReviewData target = new ReviewData();
-    private final PK pk = PK.fromLong(100L);
-    private final String comment = "comment";
-    private final Date creationTime = new Date();
-    private final String headline = "head line";
-    private final Double rating = 5.0;
-    private final String alias = "alias";
-    private final UserModel user = new UserModel();
     private final Integer helpfulnessRating= 10;
 
     @Before
     public void setUp() {
-        populator.setPrincipalConverter(principalConverter);
-
-        when(source.getPk()).thenReturn(pk);
-        when(source.getComment()).thenReturn(comment);
-        when(source.getCreationtime()).thenReturn(creationTime);
-        when(source.getHeadline()).thenReturn(headline);
-        when(source.getRating()).thenReturn(rating);
-        when(source.getAlias()).thenReturn(alias);
-        when(source.getUser()).thenReturn(user);
         when(source.getHelpfulnessRating()).thenReturn(helpfulnessRating);
-
-        when(principalConverter.convert(user)).thenReturn(new PrincipalData());
     }
 
     @Test
     public void testPopulate_Success() {
         populator.populate(source, target);
-        assertEquals(pk.getLongValueAsString(), target.getId());
-        assertEquals(comment, target.getComment());
-        assertEquals(creationTime, target.getDate());
-        assertEquals(headline, target.getHeadline());
-        assertEquals(rating, target.getRating());
-        assertEquals(alias, target.getAlias());
         assertEquals(helpfulnessRating, target.getHelpfulnessRating());
-        assertNotNull(target.getPrincipal());
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testPopulate_SourceNull() {
-        populator.populate(null, target);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testPopulate_TargetNull() {
-        populator.populate(source, null);
     }
 }
