6bad762f1238ff13541591089fa2ab7b69211617
joao-caillot-ey <joao.caillot@br.ey.com>
2026-03-10 10:33:55 -0300

    [US-00] feat: Ajustes de sonar e testes unitários

--- hybris/bin/custom/pococc/src/br/com/pococc/occ/controllers/PocProductsController.java
+++ hybris/bin/custom/pococc/src/br/com/pococc/occ/controllers/PocProductsController.java
@@ -2,13 +2,11 @@ package br.com.pococc.occ.controllers;
 
 import br.com.poc.occ.dto.product.PocProductEngagementSummaryWsDTO;
 import br.com.pocfacades.product.PocProductFacade;
-import br.com.pocfacades.review.PocReviewFacade;
 import br.com.pocfacades.product.data.PocProductEngagementSummaryInfoData;
+import br.com.pocfacades.review.PocReviewFacade;
 import de.hybris.platform.commercefacades.product.ProductFacade;
 import de.hybris.platform.commercefacades.product.data.ReviewData;
 import de.hybris.platform.commerceservices.request.mapping.annotation.RequestMappingOverride;
-import de.hybris.platform.commercewebservices.core.product.data.ReviewDataList;
-import de.hybris.platform.commercewebservicescommons.dto.product.ReviewListWsDTO;
 import de.hybris.platform.commercewebservicescommons.dto.product.ReviewWsDTO;
 import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.webservicescommons.swagger.ApiBaseSiteIdParam;
